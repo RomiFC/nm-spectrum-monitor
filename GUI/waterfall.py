06fce5b123db383ead656204ef024fdb5cf8a41f
Remy <117617803+RomiFC@users.noreply.github.com>
2025-12-04 16:53:34 -0700

    gui: changed waterfall plot file structure to RECEIVER/YEAR/MONTH/FILE #32 and #33

--- GUI/waterfall.py
+++ GUI/waterfall.py
@@ -13,6 +13,21 @@ from collections import Counter
 
 from tracedata import *
 
+def _mkdir(path: str, subfolder: str | list[str] | tuple[str]):
+    if type(subfolder) == str:
+        _dir = os.path.join(path, subfolder)
+    else:
+        _dir = os.path.join(path, *subfolder)
+    try:
+        os.makedirs(_dir)
+        logging.waterfall(f"Folder(s) '{subfolder}' created successfully in {path}")
+    except FileExistsError:
+        pass
+    except OSError as e:
+        logging.waterfall(f"Error creating folder: {e}")
+    
+    return _dir
+
 def makeWaterfalls(frompath, topath, threshold = 100, tz = 'US/Mountain', filetype = '.png', dpi=600):
     """Searches for csv files located in `frompath`, and if there are an amount of csvs with a unique date and receiver information
     in the file name above `threshold`,make a waterfall plot with them. The plot is saved in `topath` as `filetype` and the
@@ -36,14 +51,7 @@ def makeWaterfalls(frompath, topath, threshold = 100, tz = 'US/Mountain', filety
     # import data
     trace_files = getAllCsvFiles(frompath)
     # Make a directory for original csvs to move to
-    archivedir = os.path.join(topath, 'Archived')
-    try:
-        os.mkdir(archivedir)
-        logging.waterfall(f"Folder 'Archived' created successfully {topath}.")
-    except FileExistsError:
-        pass
-    except OSError as e:
-        logging.waterfall(f"Error creating folder: {e}")
+    archivedir = _mkdir(topath, 'Archived')
 
     # Make list of all dates in csv files
     for file_name in trace_files:
@@ -51,20 +59,22 @@ def makeWaterfalls(frompath, topath, threshold = 100, tz = 'US/Mountain', filety
         if hasDate:
             date = hasDate.group(1)
             dateList.append(date)
+    
     # Count the amount of each unique date TODO: check receiver info from file name
     dateCount = Counter(dateList)
     # If the count is above the threshold, make note of that date in datesToProcess
     for key in dateCount:
         if dateCount[key] > CSV_THRESHOLD:
             datesToProcess.append(key)
+    
     # This iteration occurs once for every date in datesToProcess (Once for every date above CSV_THRESHOLD)
     while datesToProcess:
-        x = []
-        y = []
-        z = []
-        dateToProcess = datesToProcess[0]
-        receivers = []
-        receiversToProcess = []
+        x = []                                  # Frequency (length n is number of points)
+        y = []                                  # Time (length m is number of csvs)
+        z = []                                  # Amplitude (shape is m by n)
+        dateToProcess = datesToProcess[0]       # Date in YYYY-MM-DD format
+        receivers = []                          # All receivers present in csvDate
+        receiversToProcess = []                 # All receivers with a count above CSV_THRESHOLD in csvData
         # Make a counter for the different receivers present in the csvs of this date
         for file_name in trace_files:
             hasDate = re.search(DATE_REGEX, file_name)
@@ -79,17 +89,14 @@ def makeWaterfalls(frompath, topath, threshold = 100, tz = 'US/Mountain', filety
                 receiversToProcess.append(key)
         # For each receiver in receiversToProcess (for this date), parse those csvs, move them, and generate the waterfall plot
         for receiver in receiversToProcess:
+            # Extract date information
+            _year = dateToProcess.split('-')[0]
+            _month = dateToProcess.split('-')[1]
             # Make directory for specific receiver/date combination
-            dirName = f'{receiver}-{dateToProcess}-WATERFALL'
-            moveToDir = os.path.join(archivedir, dirName)
-            try:
-                os.mkdir(moveToDir)
-                logging.waterfall(f"Folder '{dirName}' created successfully in {archivedir}.")
-            except FileExistsError:
-                pass
-            except OSError as e:
-                logging.waterfall(f"Error creating folder: {e}")
-            # Iterate through all csv file names, if they match date and receiver, parse them
+            _filename = f'{receiver}-{dateToProcess}-WATERFALL'
+            moveToDir = _mkdir(archivedir, (receiver, _year, _month, _filename))
+
+            # Iterate through all csv file names again, if they match date and receiver, parse them
             for file_name in trace_files:
                 hasDate = re.search(DATE_REGEX, file_name)
                 csvDate = hasDate.group(1)
@@ -108,25 +115,23 @@ def makeWaterfalls(frompath, topath, threshold = 100, tz = 'US/Mountain', filety
                     try:
                         shutil.move(file_name_joined, moveToDir)
                     except FileNotFoundError:
-                        print(f"Error: The source file '{file_name_joined}' was not found.")
+                        logging.waterfall(f"Error: The source file '{file_name_joined}' was not found.")
+                        return
                     except PermissionError:
-                        print(f"Error: Permission denied to move '{file_name_joined}'.")
+                        logging.waterfall(f"Error: Permission denied to move '{file_name_joined}'.")
+                        return
                     except Exception as e:
-                        print(f"An unexpected error occurred: {e}")
+                        logging.waterfall(f"An unexpected error occurred: {e}")
+                        return
+            
             # GENERATE WATERFALL PLOT
-            savefigdir  = os.path.join(topath, 'Waterfall-Plots')
-            try:
-                os.mkdir(savefigdir)
-                logging.waterfall(f"Folder '{savefigdir}' created successfully in the current working directory.")
-            except FileExistsError:
-                pass
-            except OSError as e:
-                logging.waterfall(f"Error creating folder: {e}")
-            savefigpath = os.path.join(savefigdir, dirName + filetype)
+            wfplotdir = _mkdir(topath, 'Waterfall-Plots')
+            wfplotfullpath = _mkdir(wfplotdir, (receiver, _year, _month))
+            wfplotfullpathandfilename = os.path.join(wfplotfullpath, _filename + filetype)
             # plot
             fig, ax = plt.subplots(layout='constrained')
             mesh = ax.pcolormesh(x, y, z, shading='nearest', vmin=-80, vmax=-30)
-            ax.set_title(f'{dirName}')
+            ax.set_title(f'{_filename}')
             ax.set_xlabel("Frequency (Hz)")
             ax.set_ylabel(f'Time ({TIMEZONE.zone})')
             ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
@@ -136,25 +141,15 @@ def makeWaterfalls(frompath, topath, threshold = 100, tz = 'US/Mountain', filety
             ax.yaxis.set_major_formatter(mdates.DateFormatter('%H:%M', tz=TIMEZONE))
             ax.invert_yaxis()
             plt.colorbar(mesh, label='Magnitude (dBm)')
-            plt.savefig(savefigpath, dpi=dpi)
-            logging.waterfall(f'File {dirName + filetype} successfully saved to {topath}')
+            plt.savefig(wfplotfullpathandfilename, dpi=dpi)
+            logging.waterfall(f'File {_filename + filetype} successfully saved to {topath}')
+
+            # GENERATE WATERFALL PLOTLY HTML
+
             # GENERATES AVERAGE CSV
-            saveavgdir  = os.path.join(topath, 'Averages')
-            saveavgdriftdir = os.path.join(saveavgdir, 'DRIFT')
-            try:
-                os.mkdir(saveavgdir)
-                logging.waterfall(f"Folder '{saveavgdir}' created successfully in the current working directory.")
-            except FileExistsError:
-                pass
-            except OSError as e:
-                logging.waterfall(f"Error creating folder: {e}")
-            try:
-                os.mkdir(saveavgdriftdir)
-                logging.waterfall(f"Folder '{saveavgdriftdir}' created successfully in the current working directory.")
-            except FileExistsError:
-                pass
-            except OSError as e:
-                logging.waterfall(f"Error creating folder: {e}")
+            avgdir = _mkdir(topath, 'Averages')
+            fullavgdir = _mkdir(topath, ('Averages', receiver, _year, _month))
+            fullavgdriftdir = _mkdir(fullavgdir, ('DRIFT', receiver, _year, _month))
             average = np.array((np.array(x[0][:]), np.mean(z, axis=0)))
             # Create pandas dataframe by using the last collected trace header as the average trace header
             datarow = pd.DataFrame({'index': ['DATA',], 'Value': [np.nan,]})
@@ -170,13 +165,14 @@ def makeWaterfalls(frompath, topath, threshold = 100, tz = 'US/Mountain', filety
             # Recreate scan name with 'D' in type to denote drift format
             AvgTraceCsvDriftName = AvgTrace.drift.scan_name + '.csv'
             # Join file paths and file names
-            AvgTraceCsvNameJoined = os.path.join(saveavgdir, AvgTraceCsvName)
-            AvgTraceCsvDriftNameJoined = os.path.join(saveavgdriftdir, AvgTraceCsvDriftName)
+            AvgTraceCsvNameJoined = os.path.join(fullavgdir, AvgTraceCsvName)
+            AvgTraceCsvDriftNameJoined = os.path.join(fullavgdriftdir, AvgTraceCsvDriftName)
             # Create csv files
             AvgTrace.trace.to_csv(AvgTraceCsvNameJoined, index=False, header=False)
-            logging.waterfall(f'File {AvgTraceCsvName} successfully saved to {saveavgdir}')
+            logging.waterfall(f'File {AvgTraceCsvName} successfully saved to {fullavgdir}')
             AvgTraceDrift.to_csv(AvgTraceCsvDriftNameJoined, index=False)
-            logging.waterfall(f'File {AvgTraceCsvDriftName} successfully saved to {saveavgdriftdir}')
+            logging.waterfall(f'File {AvgTraceCsvDriftName} successfully saved to {fullavgdriftdir}')
+
         datesToProcess.remove(dateToProcess)
 
     logging.waterfall('No more plots to generate.')
